0aff59256272e9e332e781ea19f787c4b12441b9
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-03-26 23:24:30 +0100

    Add menu

---
+++ source.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    // CREATE - Vers hozzáadaása
+    // READ - Versek listázása
+    // UPDATE - Vers módosítása
+    // DELETE - Vers törlése
+
+    while (1)
+    {
+        printf("Menu\n");
+        printf("[1] - Vers hozzaadasa \n");
+        printf("[2] - Versek listazasa \n");
+        printf("[3] - Vers modositasa \n");
+        printf("[4] - Vers torlese \n");
+        printf("[5] - Kilepes \n");
+        printf("Valsztott menu: ");
+
+    }
+
+    return 0;
+}
