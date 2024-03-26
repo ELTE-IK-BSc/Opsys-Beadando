b61429d85214723b348e3c0220c1819eafa1c8da
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-03-26 23:51:34 +0100

    Add menu logic

--- source.c
+++ source.c
@@ -9,14 +9,41 @@ int main()
 
     while (1)
     {
-        printf("Menu\n");
+        printf("\n");
+        printf("---------Menu---------\n");
         printf("[1] - Vers hozzaadasa \n");
         printf("[2] - Versek listazasa \n");
         printf("[3] - Vers modositasa \n");
         printf("[4] - Vers torlese \n");
         printf("[5] - Kilepes \n");
-        printf("Valsztott menu: ");
+        printf("----------------------\n");
 
+        printf("Valsztott menu: ");
+        printf("\n");
+
+        int menuitem;
+        scanf("%d", &menuitem);
+        switch (menuitem)
+        {
+        case 1:
+            printf("Vers hozzaadva \n");
+            break;
+        case 2:
+            printf("Versek: \n");
+            break;
+        case 3:
+            printf("Vers modositva \n");
+            break;
+        case 4:
+            printf("Vers torlve \n");
+            break;
+        case 5:
+            printf("Kilepes \n");
+            return;
+
+        default:
+            break;
+        }
     }
 
     return 0;
