244cd9fef6b59912e1ade320eeffa0f1bb7cd00e
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-03-27 13:04:44 +0100

    Add spacing to menu

--- source.c
+++ source.c
@@ -18,30 +18,38 @@ int main()
         printf("[5] - Kilepes \n");
         printf("----------------------\n");
 
-        printf("Valsztott menu: ");
-        printf("\n");
+        printf("Valsztott menupont: ");
 
         int menuitem;
         scanf("%d", &menuitem);
         switch (menuitem)
         {
         case 1:
+            printf("\n");
+
             printf("Vers hozzaadva \n");
             break;
         case 2:
+            printf("\n");
             printf("Versek: \n");
             break;
         case 3:
+            printf("\n");
+
             printf("Vers modositva \n");
             break;
         case 4:
+            printf("\n");
             printf("Vers torlve \n");
             break;
         case 5:
+            printf("\n");
             printf("Kilepes \n");
-            return;
+            return 0;
 
         default:
+            printf("\n");
+            printf("Isemretlen menupont probald ujra! \n");
             break;
         }
     }
